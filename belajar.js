18f77a4bc32a35a157de30d697c6475472c60ce2
yaelahky <kiky.lenovo@gmail.com>
2020-08-28 08:54:53 +0700

    init

---
+++ belajar.js
@@ -0,0 +1,23 @@
+// function getName(parameter) {
+//     // let hasil = 1+1
+//     // const hasil = 1+1
+//     // hasil = 0
+//     // return typeof hasil
+//     // const namaHewan = [
+//     //     {
+//     //         name: 'Kucing'
+//     //     },
+//     //     {
+//     //         name: 'Ular'
+//     //     }
+//     // ]
+
+//     // namaHewan.map((item, index) => {
+//     //     console.log(index, item.name)
+//     // })
+    // fetch('https://jsonplaceholder.typicode.com/posts')
+    // .then(response => response.json())
+    // .then(result => console.log(result))
+// }
+
+// console.log(getName())
